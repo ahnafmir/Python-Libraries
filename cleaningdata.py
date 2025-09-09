d7321a7ab6f20e55371b899b7abb786e4f51457d
ahnufmir <mirahnaf55@gmail.com>
2025-09-09 20:29:39 +0500

    Numpy + Pandas Practise

--- cleaningdata.py
+++ cleaningdata.py
@@ -7,12 +7,27 @@ df = pd.read_csv("messydatafile.csv")
 # print("Mistakes in file:")
 # print(df.isnull().sum())
 
-print(df['Rating'].dtype)
-print(df['Founded'].dtype)
-print(df['Revenue'].dtype)
+df_cat = df.select_dtypes(np.object_)
+print(df_cat.head)
+# df_num = df.select_dtypes(np.number)
+# df_float = df.select_dtypes(np.float64)
 
-df["Rating"].fillna(df["Rating"].median())
-df["Founded"].fillna(df["Founded"].mean())
+print("Object Columns: \n",df_cat)
+# print("Numeric Columns: \n",df_num)
+# print("Float Columns: \n",df_float)
+
+print(df["Size"].dtype)
+missing_tokens = ["-", "--", "NA", "N/A", "n/a", "None", "none", "", "nan", "NaN", -1, -99]
+df["Headquarters"] = df["Headquarters"].replace(missing_tokens, np.nan)
+#Option 1
+# if "Rating" in df.columns:
+#     df["Rating"] =  df["Rating"].fillna(df["Rating"].median())
+
+# if "Founded" in df.columns:
+#     df["Founded"] = df["Founded"].fillna(df["Founded"].round().mean())
+
+
+#Option 2
 # df["Revenue"].fillna(df["Revenue"].mean(), inplace= True)
 
 # df.replace([np.inf, -np.inf], np.nan, inplace=True)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-city = input("Enter you city name: ")
+df_cat = df.select_dtypes(np.object_)
+# df_num = df.select_dtypes(np.number)
+# df_float = df.select_dtypes(np.float64)
+
+# print("Object Columns: \n",df_cat)
+# print("Numeric Columns: \n",df_num)
+# print("Float Columns: \n",df_float)
